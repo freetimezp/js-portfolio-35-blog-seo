59a632f7e6d293b848fc80fe7002a04ee2001f7a
freetimezp <kraftukrnet@ukr.net>
2023-12-09 23:10:08 +0200

    12. js-portfolio-35-blog-seo. About Page Component.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AboutCiverSection from '../../components/About/AboutCiverSection';
+import Skills from '../../components/About/Skills';
+import Link from 'next/link';
+
+const AboutPage = () => {
+    return (
+        <>
+            <AboutCiverSection />
+            <Skills />
+
+            <h2 className='mt-8 font-semibold text-2xl self-start mx-20 text-dark'>
+                Have a project in mind? Reach out to me 📞 from {" "}
+                <Link href="/contact" className='!underline underline-offset-2'>here</Link> and let's make it happen.
+            </h2>
+        </>
+    );
+};
+
+export default AboutPage;
