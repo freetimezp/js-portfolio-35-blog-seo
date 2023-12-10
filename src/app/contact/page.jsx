2f5e771554d8c1aa44f5477348423b6d76934a52
freetimezp <kraftukrnet@ukr.net>
2023-12-10 21:48:38 +0200

    13. js-portfolio-35-blog-seo. Contact Page Component.

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import LottieAnimation from '../../components/Contact/LottieAnimation';
+import ContactForm from '../../components/Contact/ContactForm';
+
+const ContactPage = () => {
+    return (
+        <section className='w-full h-[75vh] border-b-2 border-solid border-dark flex 
+            items-center justify-center text-dark'>
+            <div className='w-2/5 inline-block h-full border-r-2 border-solid border-dark'>
+                <LottieAnimation />
+            </div>
+            <div className='w-3/5 flex flex-col items-start justify-center px-16 pb-8'>
+                <h2 className='font-bold capitalize text-4xl'>
+                    Let's Connect!
+                </h2>
+                <ContactForm />
+            </div>
+        </section>
+    );
+};
+
+export default ContactPage;
---
+++ src/components/Contact/ContactForm.jsx
@@ -0,0 +1,36 @@
+"use client"
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function ContactForm() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className='mt-12 text-xl font-medium leading-relaxed 
+            font-in'>
+            Hello! My name is{" "}
+            <input type="text" placeholder="Your name" {...register("name", { required: true })}
+                className='outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center 
+                placeholder:text-lg border-b border-gray focus:border-gray bg-transparent' />
+            and I want to discuss a potential project. You can email me at{" "}
+            <input type="email" placeholder="Your @email" {...register} className='outline-none 
+                border-0 p-0 mx-2 focus:ring-0 placeholder:text-center 
+                placeholder:text-lg border-b border-gray focus:border-gray bg-transparent' />
+            or reach out to me on{" "}
+            <input type="tel" placeholder="Your phone number" {...register("phone number", {})}
+                className='outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center 
+                placeholder:text-lg border-b border-gray focus:border-gray bg-transparent'/>
+            Here are some details about my project: <br />
+            <textarea {...register("project details", {})} rows={3}
+                placeholder="My project is about..."
+                className='outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-lg 
+                border-b border-gray focus:border-gray bg-transparent w-full' />
+
+            <input type="submit" placeholder='Send' className='mt-8 font-medium inline-block 
+                uppercase text-xl py-3 px-8 border-2 border-solid border-dark rounded 
+                cursor-pointer' value="Send Message" />
+        </form >
+    );
+}
